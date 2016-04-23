7c1abe857972e001b35a1daf620a36da303de4a2
yanglu <990128987@qq.com>
2016-04-23 20:23:40 +0800

    add jpg_spider.py

--- jpg_spider.py
+++ jpg_spider.py
@@ -1,21 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-i = 0
+index = 0
 
 
 # 保存图片
 def save_jpg(res_url):
-    global i
+    global index
     html = BeautifulSoup(requests.get(res_url).text)
     for link in html.find_all('a', {'class': 'view_img_link'}):
-        with open('{}.jpg'.format(i), 'wb') as jpg:
+        with open('{}.jpg'.format(index), 'wb') as jpg:
             jpg.write(requests.get(link.get('href')).content)
-        i += 1
+        index += 1
 
 
 #  抓取煎蛋妹子图片，默认抓取5页
 if __name__ == '__main__':
+
     url = 'http://jandan.net/ooxx'
     for i in range(0, 5):
         save_jpg(url)
