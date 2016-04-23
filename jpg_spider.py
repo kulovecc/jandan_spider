63dfb650f06f963ad2f8106bdee5a791b2be9943
yanglu <990128987@qq.com>
2016-04-23 23:56:00 +0800

    增加gif格式支持

--- jpg_spider.py
+++ jpg_spider.py
@@ -9,15 +9,14 @@ def save_jpg(res_url):
     global index
     html = BeautifulSoup(requests.get(res_url).text)
     for link in html.find_all('a', {'class': 'view_img_link'}):
-        with open('{}.jpg'.format(index), 'wb') as jpg:
+        with open('{}.{}'.format(index, link.get('href')[len(link.get('href'))-3: len(link.get('href'))]), 'wb') as jpg:
             jpg.write(requests.get(link.get('href')).content)
         index += 1
 
 
 #  抓取煎蛋妹子图片，默认抓取5页
 if __name__ == '__main__':
-
     url = 'http://jandan.net/ooxx'
-    for i in range(0, 5):
+    for i in range(0, 10):
         save_jpg(url)
         url = BeautifulSoup(requests.get(url).text).find('a', {'class': 'previous-comment-page'}).get('href')
