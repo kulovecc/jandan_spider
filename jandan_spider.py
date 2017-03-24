e6f7578df45c0bac8755726a5742103a50509d74
louis <990128987@qq.com>
2017-03-24 22:01:49 +0800

    fix bug.

--- jandan_spider.py
+++ jandan_spider.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 index = 0
-headers={'referer':'http://jandan.net/', 'user-agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+headers = {'referer': 'http://jandan.net/', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+
 
 # 保存图片
 def save_jpg(res_url):
@@ -10,7 +11,8 @@ def save_jpg(res_url):
     html = BeautifulSoup(requests.get(res_url, headers=headers).text)
     for link in html.find_all('a', {'class': 'view_img_link'}):
         with open('{}.{}'.format(index, link.get('href')[len(link.get('href'))-3: len(link.get('href'))]), 'wb') as jpg:
-            jpg.write(requests.get(link.get('href')).content)
+            jpg.write(requests.get("http:" + link.get('href')).content)
+        print("正在抓取第%s条数据" % index)
         index += 1
 
 
